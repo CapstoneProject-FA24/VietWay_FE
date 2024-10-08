22a9a6365616d95460f01c3188bce53e673d8c88
Hieu-03 <hieupnse171321@fpt.edu.vn>
2024-10-08 11:28:00 +0700

    Create Statuses.jsx

---
+++ src/hooks/Statuses.jsx
@@ -0,0 +1,49 @@
+export const Gender = {
+  Male: 0,
+  Female: 1,
+  Other: 2
+};
+
+export const UserRole = {
+  Customer: 0,
+  Staff: 1,
+  Manager: 2,
+  Admin: 3
+};
+
+export const TourTemplateStatus = {
+  Draft: 0,
+  Pending: 1,
+  Approved: 2,
+  Rejected: 3
+};
+
+export const AttractionStatus = {
+  Draft: 0,
+  Pending: 1,
+  Approved: 2,
+  Rejected: 3
+};
+
+export const TourStatus = {
+  Pending: 0,
+  Rejected: 1,
+  Scheduled: 2,
+  Closed: 3,
+  OnGoing: 4,
+  Completed: 5,
+  Cancelled: 6
+};
+
+export const BookingStatus = {
+  Pending: 0,
+  Confirmed: 1,
+  Cancelled: 2,
+  Completed: 3
+};
+
+export const PaymentStatus = {
+  Pending: 0,
+  Paid: 1,
+  Failed: 2
+};
