2b30341c33b62bbc60aa1a494a6bea8e0b422abf
Hieu-03 <hieupnse171321@fpt.edu.vn>
2024-10-18 00:31:27 +0700

    fix header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, Typography, Button, InputBase, Box, IconButton, Menu, 
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { styled, alpha } from '@mui/material/styles';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 /* const Search = styled('div')(({ theme }) => ({
   position: 'relative', borderRadius: theme.shape.borderRadius * 3,
@@ -38,11 +38,23 @@ const StyledButton = styled(Button)(({ theme }) => ({
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
+  const [scrollY, setScrollY] = useState(0);
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     setIsLoggedIn(!!token);
+
+    const handleScroll = () => {
+      setScrollY(window.pageYOffset);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   const handleMenu = (event) => {
@@ -66,18 +78,23 @@ const Header = () => {
   };
 
   return (
-    <AppBar position="fixed" sx={{ backgroundColor: 'white', boxShadow: 1, width: '100%' }}>
+    <AppBar
+      position="fixed"
+      sx={{
+        backgroundColor: location.pathname === '/trang-chu' && scrollY === 0 ? 'transparent' : 'white',
+        boxShadow: location.pathname === '/trang-chu' && scrollY === 0 ? 0 : 1,
+        width: '100%',
+        transition: 'background-color 0.3s, box-shadow 0.3s',
+      }}
+    >
       <Toolbar sx={{ justifyContent: 'space-between' }}>
         <Typography variant="h6" component={Link} to="/trang-chu" sx={{ color: 'text.primary' }}>
-          <img src="/logo2_color.png" alt="Logo" style={{ height: '55px', marginTop: '15px' }} />
+          <img src={location.pathname === '/trang-chu' && scrollY === 0 ? "/logo2.png" : "/logo2_color.png"} alt="Logo" style={{ height: '55px', marginTop: '15px' }} />
         </Typography>
         
         {/* <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
           <Search sx={{ flexGrow: 1 }}>
-            <StyledInputBase
-              placeholder="Tìm kiếm..."
-              inputProps={{ 'aria-label': 'search' }}
-            />
+            <StyledInputBase placeholder="Tìm kiếm..." inputProps={{ 'aria-label': 'search' }} />
           </Search>
           <IconButton sx={{ p: '10px', marginTop: '10px', marginLeft: '-15px' }} aria-label="search">
             <SearchIcon />
@@ -86,64 +103,62 @@ const Header = () => {
         
         <Box sx={{ display: 'flex', marginLeft: 3, marginRight: 3 }}>
           <StyledButton color="inherit" component={Link} to="/trang-chu" sx={{ color: 'text.primary', textTransform: 'none',
-              fontWeight: location.pathname === '/trang-chu' ? 'bold' : 'normal' }}>
+              fontWeight: location.pathname === '/trang-chu' ? 'bold' : 'normal', 
+              color: location.pathname === '/trang-chu' && scrollY === 0 ? 'white' : 'black' }}>
             Trang chủ
           </StyledButton>
           <StyledButton
-            color="inherit"
-            component={Link}
-            to="/tinh-thanh"
+            color="inherit" component={Link} to="/tinh-thanh"
             sx={{
-              color: 'text.primary',
-              textTransform: 'none',
-              fontWeight: location.pathname === '/tinh-thanh' ? 'bold' : 'normal'
+              color: 'text.primary', textTransform: 'none',
+              fontWeight: location.pathname === '/tinh-thanh' ? 'bold' : 'normal', 
+              color: location.pathname === '/trang-chu' && scrollY === 0 ? 'white' : 'black'
             }}
           >
             Tỉnh thành
           </StyledButton>
           <StyledButton
-            color="inherit"
-            component={Link}
-            to="/diem-tham-quan"
+            color="inherit" component={Link} to="/diem-tham-quan"
             sx={{
-              color: 'text.primary',
-              textTransform: 'none',
-              fontWeight: location.pathname === '/diem-tham-quan' ? 'bold' : 'normal'
+              color: 'text.primary', textTransform: 'none',
+              fontWeight: location.pathname === '/diem-tham-quan' ? 'bold' : 'normal', 
+              color: location.pathname === '/trang-chu' && scrollY === 0 ? 'white' : 'black'
             }}
           >
             Điểm tham quan
           </StyledButton>
           <StyledButton
-            color="inherit"
-            component={Link}
-            to="/tour-du-lich"
+            color="inherit" component={Link} to="/tour-du-lich"
             sx={{
-              color: 'text.primary',
-              textTransform: 'none',
-              fontWeight: location.pathname === '/tour-du-lich' ? 'bold' : 'normal'
+              color: 'text.primary', textTransform: 'none',
+              fontWeight: location.pathname === '/tour-du-lich' ? 'bold' : 'normal', 
+              color: location.pathname === '/trang-chu' && scrollY === 0 ? 'white' : 'black'
             }}
           >
             Tour du lịch
           </StyledButton>
           <StyledButton
-            color="inherit"
-            component={Link}
-            to="/bai-viet"
-            sx={{
-              color: 'text.primary',
-              textTransform: 'none',
-              fontWeight: location.pathname === '/bai-viet' ? 'bold' : 'normal'
+            color="inherit" component={Link} to="/bai-viet"
+            sx={{ color: 'text.primary', textTransform: 'none',
+              fontWeight: location.pathname === '/bai-viet' ? 'bold' : 'normal', 
+              color: location.pathname === '/trang-chu' && scrollY === 0 ? 'white' : 'black'
             }}
           >
             Bài viết
           </StyledButton>
+          <StyledButton
+            color="inherit" component={Link} to="/su-kien"
+            sx={{ color: 'text.primary', textTransform: 'none',
+              fontWeight: location.pathname === '/su-kien' ? 'bold' : 'normal', 
+              color: location.pathname === '/trang-chu' && scrollY === 0 ? 'white' : 'black'
+            }}
+          >
+            Sự kiện
+          </StyledButton>
           {/* <StyledButton
-            color="inherit"
-            component={Link}
-            to="/lien-he"
+            color="inherit" component={Link} to="/lien-he"
             sx={{
-              color: 'text.primary',
-              textTransform: 'none',
+              color: 'text.primary', textTransform: 'none',
               fontWeight: location.pathname === '/lien-he' ? 'bold' : 'normal'
             }}
           >
@@ -155,28 +170,15 @@ const Header = () => {
           {isLoggedIn ? (
             <>
               <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="main.grey"
-                sx={{ border: '3px solid grey' }}
+                size="large" aria-label="account of current user" aria-controls="menu-appbar"
+                aria-haspopup="true" onClick={handleMenu}  sx={{ border: location.pathname === '/trang-chu' && scrollY === 0  ? '3px solid white' : '3px solid grey' }}
               >
-                <AccountCircleIcon/>
+                <AccountCircleIcon sx={{ color: location.pathname === '/trang-chu' && scrollY === 0  ? "white" : "grey" }}/>
               </IconButton>
               <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
+                id="menu-appbar" anchorEl={anchorEl}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }} keepMounted
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
               >
